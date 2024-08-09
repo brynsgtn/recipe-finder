77ef8ff704e3bed8c363e3fcb53f9b372ffef0f4
brynsgtn <bryansuguitan17@gmail.com>
2024-08-09 10:54:02 -0500

    Fixing deployment issues.

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <MantineProvider withGlobalStyles withNormalizeCSS>
     <React.StrictMode>
-      <BrowserRouter basename={"/recipe-finder/"}>
         <App />
-      </BrowserRouter>
     </React.StrictMode>
   </MantineProvider>
 
